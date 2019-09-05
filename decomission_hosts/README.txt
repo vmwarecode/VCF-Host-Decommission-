f9c8c8d8b3f16031faaca08f352e96b983137762
VMware Developer Center <noreply@vmware.com>
2019-09-05 08:57:52 +0000

    Initial commit by dksudharshan

---
+++ decomission_hosts/README.txt
@@ -0,0 +1,13 @@
+Decomsission hosts:
+
+The following data is required
+
+FQDN of each host
+
+The host must not be assigned to a domain i.e "status" must be "UNASSIGNED_USEABLE".
+
+TIP
+Refer to: Get the Hosts to fetch the hosts with the required "status"
+
+Usage:
+    python decomission_hosts.py <hostname> <username> <password> <fqdn1> <fqdn2> ..
---
+++ decomission_hosts/decomission_hosts.py
@@ -0,0 +1,44 @@
+#Decomission hosts.
+#Decomissions hosts gives as parametres.
+import requests
+import json
+import sys
+import time
+
+def get_help():
+    help_description = '''\n\t\t----Decomission hosts----
+    Usage:
+    python decomission_hosts.py <hostname> <username> <password> <fqdn1> <fqdn2> ..\n Refer to documentation for more detais\n'''
+    print help_description
+
+def decomission_hosts():
+    arguments = sys.argv
+    if(len(arguments) < 4):
+        get_help()
+        return
+    hostname = 'https://'+arguments[1]
+    username = arguments[2]
+    password = arguments[3]
+    headers = {'Content-Type': 'application/json'}
+    url = hostname+'/v1/hosts'
+    data = []
+    for fqdn in range(3,len(arguments)):
+        data.append({'fqdn':arguments[fqdn]})
+    response = requests.delete(url, headers=headers,json = data,auth=(username, password))
+    if(response.status_code == 202):
+        data = json.loads(response.text)
+        task_id = data['id']
+        status = data['status']
+        url = hostname+'/v1/tasks/'+task_id
+        print 'Decomissioning hosts...'
+        while(status == 'In Progress'):
+            status = json.loads(requests.get(url, headers=headers,auth=(username, password)).text)['status']
+            time.sleep(5)
+        print 'Decomissioning of hosts is '+status
+    else:
+        print "Error decomissioning hosts."
+        response = json.loads(response.text)
+        print json.dumps(response,indent=4, sort_keys=True)
+        exit(1)
+decomission_hosts()
+
